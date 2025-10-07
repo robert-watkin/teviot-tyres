fcb46c8e5193a014085f034ac147f2b8d05959f9
Robert Watkin <robert.watkin12@gmail.com>
2025-10-07 23:54:36 +0100

    feat: admin account seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Create admin test account
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@test.com',
+            'password' => 'password',
+            'role' => 'admin',
         ]);
     }
 }
